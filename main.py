0d900c1de096ddb3f08f194ef8fc4f57d57ecbd7
jenny xu <jingwenxu13@gmail.com>
2025-03-20 11:34:42 -0700

    hello

--- main.py
+++ main.py
@@ -4,11 +4,8 @@ def greet():
 def farewell():
     print("Goodbye, World!")
 
-<<<<<<< HEAD
 
 def name():
     print('what is your name?')
-=======
-def chat():
-    print("yes")
->>>>>>> beeda71c4d3bc1ad043679563bcb8e147b516e01
+
+print("hello")
